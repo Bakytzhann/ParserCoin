8a5f9fca6e6edff2ad7af967a03b5cf130991f18
Bakytzhann <aisabakytzhan@gmail.com>
2020-06-05 21:17:04 +0600

    commit1 - Парсер однопоточный

---
+++ lowermain.py
@@ -0,0 +1,16 @@
+"""
+1.Парсер однопоточный
+2.Замер времени
+3.multiprocessing Pool
+4.Замер времени
+5.Экспорт данных в csv
+"""
+
+def main():
+    print('Hello')
+   #https://coinmarketcap.com/all/views/all/
+    
+
+if __name__ == '__main__':
+    main()
+
